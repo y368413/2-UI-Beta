cf5ad2bc9f7b703ab587beb9d698d30b24c426ad
y368413 <45956954@.com>
2019-09-04 17:51:49 +0800

    背包大改

--- Interface/Addons/Combuctor/common/features/commands.lua
+++ Interface/Addons/Combuctor/common/features/commands.lua
@@ -69,14 +69,12 @@ end
 --[[ Options ]]--
 
 function Addon:CreateOptionsLoader()
-	CreateFrame('Frame', nil, InterfaceOptionsFrame):SetScript('OnShow', function(self)
-		LoadAddOn(ADDON .. '_Config')
-	end)
+	CreateFrame('Frame', nil, InterfaceOptionsFrame):SetScript('OnShow', function(self) LoadAddOn(ADDON) end)
 end
 
 function Addon:ShowOptions()
-	if LoadAddOn(ADDON .. '_Config') then
+	--if LoadAddOn(ADDON .. '_Config') then
 		InterfaceOptionsFrame_OpenToCategory(ADDON)
 		InterfaceOptionsFrame_OpenToCategory(ADDON) -- sometimes once not enough
-	end
+	--end
 end
