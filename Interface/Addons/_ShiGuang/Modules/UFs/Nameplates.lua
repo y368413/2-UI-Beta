16983a6a85456445277a26f27aa25acca2a9af5a
y368413 <45956954@qq.com>
2020-02-11 22:46:24 +0800

    3.4.2

--- Interface/Addons/_ShiGuang/Modules/UFs/Nameplates.lua
+++ Interface/Addons/_ShiGuang/Modules/UFs/Nameplates.lua
@@ -756,6 +756,9 @@ function UF:PostUpdatePlates(event, unit)
 			guidToPlate[self.unitGUID] = self
 		end
 		self.npcID = M.GetNPCID(self.unitGUID)
+		
+		local blizzPlate = self:GetParent().UnitFrame
+		self.widget = blizzPlate.WidgetContainer
 	elseif event == "NAME_PLATE_UNIT_REMOVED" then
 		if self.unitGUID then
 			guidToPlate[self.unitGUID] = nil
